8ba8bea719ec8738fa7e9eaf6bd9c72cef27628f
Victor Rodriguez <victorodriguez148@gmail.com>
2020-09-29 23:48:16 +0200

    Photon class functions

--- Objects/Photon.cpp
+++ Objects/Photon.cpp
@@ -1,5 +1,23 @@
-//
-// Created by Victor Rodriguez on 29/09/2020.
-//
 
 #include "Photon.h"
+
+Photon::Photon(float x, float y, float dir_vx, float dir_vy)
+{
+    pos.x = x, pos.y = y;
+    vel.x = dir_vx, vel.y = dir_vy;
+    vel.setMag(Constants::c);
+
+    trail.setPrimitiveType(sf::LineStrip);
+    trail.append(sf::Vertex(pos, sf::Color::Yellow));
+}
+
+void Photon::move(float dx, float dy, float dt)
+{
+    pos += Vector2(dx, dy) * dt;
+    trail.append(sf::Vertex(pos, sf::Color::Yellow));
+}
+
+void Photon::draw(sf::RenderWindow& window)
+{
+    window.draw(trail);
+}
--- Objects/Photon.h
+++ Objects/Photon.h
@@ -1,9 +1,22 @@
 
 #pragma once
 
+#include "../Utilities/Vector2.h"
+#include "SFML/Graphics.hpp"
+#include "BlackHole.h"
+
 class Photon
 {
+    private:
+
+        Vector2 pos;
+        Vector2 vel;
+        sf::VertexArray trail;
+
     public:
 
+        Photon(float x, float y, float dir_vx, float dir_vy);
 
+        void move(float dx, float dy, float dt);
+        void draw(sf::RenderWindow& window);
 };
