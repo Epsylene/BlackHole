30bdecc3db3055c0c34196c5de32eb79500b4d53
xam4lor <xam4lor@gmail.com>
2020-10-02 18:34:31 +0200

    Added the getAngle() function

--- Utilities/Vector2.cpp
+++ Utilities/Vector2.cpp
@@ -73,7 +73,7 @@ float Vector2::getMag(Vector2 vec)
 
 float Vector2::getAngle(Vector2 vec1, Vector2 vec2)
 {
-    //
+    return static_cast<float>(vec2.angle() - vec1.angle());
 }
 
 float Vector2::distance(Vector2 vec1, Vector2 vec2)
